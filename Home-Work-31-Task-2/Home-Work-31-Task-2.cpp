fe36994a7daaef6037de48ed8532e033a8626ecf
igor_kartsevich <71553499+igorkartsevich@users.noreply.github.com>
2022-02-06 16:07:25 +0300

    06/02/01

--- Home-Work-31-Task-2/Home-Work-31-Task-2.cpp
+++ Home-Work-31-Task-2/Home-Work-31-Task-2.cpp
@@ -20,6 +20,7 @@ public:
     ListGraph() : verticesCounter(0) {}
 
     ListGraph(const ListGraph& other_graph) : IGraph(other_graph) {
+        verticesCounter = 0;
         list_next = other_graph.list_next;
         list_prev = other_graph.list_prev;
     }
@@ -47,17 +48,15 @@ public:
 
     virtual void GetNextVertices(int vertex, std::vector<int>& vertices) const override {
         vertices.clear();
-        if (vertex < std::max(list_next.size(), list_prev.size()))
-            for (auto i : list_next.find(vertex)->second) vertices.push_back(i);
-        else std::cout << "This Graph does not contain vertex number " << vertex;
+        if (list_next.find(vertex) == list_next.end()) std::cout << "Vertex " << vertex << " does not have next vertices";
+        else for (auto i : list_next.find(vertex)->second) vertices.push_back(i);
         return;
     }
 
     virtual void GetPrevVertices(int vertex, std::vector<int>& vertices) const override {
         vertices.clear();
-        if (vertex < std::max(list_next.size(), list_prev.size()))
-            for (auto i : list_prev.find(vertex)->second) vertices.push_back(i);
-        else std::cout << "This Graph does not contain vertex number " << vertex;
+        if (list_prev.find(vertex) == list_prev.end()) std::cout << "Vertex " << vertex << " does not have previous vertices";
+        else for (auto i : list_prev.find(vertex)->second) vertices.push_back(i);
         return;
     }
 
@@ -65,7 +64,6 @@ private:
     int verticesCounter;
     std::map<int, std::vector<int>> list_next;
     std::map<int, std::vector<int>> list_prev;
-
 };
 
 class MatrixGraph : public IGraph {
@@ -75,46 +73,88 @@ public:
     MatrixGraph() : verticesCounter(0) {}
 
     MatrixGraph(const MatrixGraph& other_graph) : IGraph(other_graph) {
-        
+        verticesCounter = 0;
+        matrix_next = other_graph.matrix_next;
+        matrix_next = other_graph.matrix_prev;
     }
 
     virtual void AddEdge(int from, int to) override {
+        bool is_adge_from_to = false;
+        if (matrix_next.find(from) != matrix_next.end())
+            if (matrix_next.find(from)->second.find(to) != matrix_next.find(from)->second.end()) is_adge_from_to = true;
+
+        bool is_from_in_vertices = (matrix_next.find(from) != matrix_next.end() || matrix_prev.find(from) != matrix_prev.end());
+        bool is_to_in_vertices =   (matrix_next.find(to)   != matrix_next.end() || matrix_prev.find(to)   != matrix_prev.end());
+
+        if (!is_adge_from_to) {
+
+            if (!is_from_in_vertices) verticesCounter++;
+            if (!is_to_in_vertices) verticesCounter++;
+            if (from == to) verticesCounter--;
+
+            matrix_next[from][to] = 1;
+            matrix_prev[to][from] = -1;
+        }
+        return;
+    }
+
+    virtual int VerticesCount() const override {
+        return verticesCounter;
+    }
+
+    virtual void GetNextVertices(int vertex, std::vector<int>& vertices) const override {
+        vertices.clear();
         
+        return;
+    }
 
-
+    virtual void GetPrevVertices(int vertex, std::vector<int>& vertices) const override {
+        /*vertices.clear();
+        if (vertex < std::max(list_next.size(), list_prev.size()))
+            for (auto i : list_prev.find(vertex)->second) vertices.push_back(i);
+        else std::cout << "This Graph does not contain vertex number " << vertex;*/
         return;
     }
 
 private:
     int verticesCounter;
-    std::map<int, std::map<int, bool>> matrix_next;
-    std::map<int, std::map<int, bool>> matrix_prev;
+    std::map<int, std::map<int, int>> matrix_next;
+    std::map<int, std::map<int, int>> matrix_prev;
 };
 
 int main()
 {
-    ListGraph g1;
+    ListGraph lg1;
 
-    g1.AddEdge(1, 2);
-    g1.AddEdge(1, 4);
-    g1.AddEdge(1, 2);
-    g1.AddEdge(100, 100);
-    g1.AddEdge(100, 100);
-    g1.AddEdge(-1, 1);
+    lg1.AddEdge(1, 2);
+    lg1.AddEdge(1, 4);
+    lg1.AddEdge(1, 2);
+    lg1.AddEdge(100, 100);
+    lg1.AddEdge(100, 100);
+    lg1.AddEdge(-1, 1);
 
-    std::cout << "This Graph contains " << g1.VerticesCount() << " vertices\n";
+    std::cout << "This Graph contains " << lg1.VerticesCount() << " vertices\n";
     
     std::vector<int> vertices;
-    g1.GetNextVertices(1, vertices);
+    lg1.GetNextVertices(14, vertices);
     for (auto i : vertices) std::cout << i << " ";
     std::cout << "\n";
-    g1.GetPrevVertices(1, vertices);
+    lg1.GetPrevVertices(1, vertices);
     for (auto i : vertices) std::cout << i << " ";
     std::cout << "\n";
 
-    ListGraph g2(g1);
-    ListGraph g3;
-    g3 = g2;
+    ListGraph lg2(lg1);
+    ListGraph lg3;
+    lg3 = lg2;
 
-    //MatrixGraph g2 = g1;
+    MatrixGraph mg1;
+
+    mg1.AddEdge(1, 2);
+    mg1.AddEdge(1, 4);
+    mg1.AddEdge(1, 2);
+    mg1.AddEdge(100, 100);
+    mg1.AddEdge(100, 100);
+    mg1.AddEdge(-1, 1);
+
+    std::cout << "This Graph contains " << mg1.VerticesCount() << " vertices\n";
 }
